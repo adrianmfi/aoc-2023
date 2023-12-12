57ae8a5a05b8996febaacfe36bff4b1caef5c41c
Adrian Meidell Fiorito <adrian@sparkel.io>
2023-12-12 15:41:11 +0100

    Day 11

---
+++ Day11Part1.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day11Part1 {
+    public static void main(String[] args) throws IOException {
+        var filePath = "./input/day11.txt";
+
+        var lines = Files.readString(Paths.get(filePath))
+                .strip()
+                .split(System.lineSeparator());
+
+        var galaxies = new ArrayList<Position>();
+        var hasRowsGalaxy = new boolean[lines.length];
+        var hasColsGalaxy = new boolean[lines[0].length()];
+
+        for (int i = 0; i < lines.length; i++) {
+            for (int j = 0; j < lines[0].length(); j++) {
+                if (lines[i].charAt(j) == '#') {
+                    galaxies.add(new Position(i, j));
+                    hasRowsGalaxy[i] = true;
+                    hasColsGalaxy[j] = true;
+                }
+            }
+        }
+        var rowsWithoutGalaxies = IntStream.range(0, hasRowsGalaxy.length).filter(i -> !hasRowsGalaxy[i])
+                .mapToObj(i -> i)
+                .collect((Collectors.toCollection(TreeSet::new)));
+        var colsWithoutGalaxies = IntStream.range(0, hasColsGalaxy.length).filter(i -> !hasColsGalaxy[i])
+                .mapToObj(i -> i)
+                .collect((Collectors.toCollection(TreeSet::new)));
+
+        var sum = 0;
+        for (int i = 0; i < galaxies.size() - 1; i++) {
+            var gal1 = galaxies.get(i);
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                var gal2 = galaxies.get(j);
+                sum += getDistanceBetween(gal1, gal2, rowsWithoutGalaxies, colsWithoutGalaxies);
+            }
+
+        }
+        System.out.println(String.format("Result %s", sum));
+
+    }
+
+    private static int getDistanceBetween(
+            Position gal1, Position gal2, TreeSet<Integer> rowsWithoutGalaxies, TreeSet<Integer> colsWithoutGalaxies) {
+        var minRow = Math.min(gal1.row(), gal2.row());
+        var maxRow = Math.max(gal1.row(), gal2.row());
+
+        var minCol = Math.min(gal1.col(), gal2.col());
+        var maxCol = Math.max(gal1.col(), gal2.col());
+
+        var rowExpansionBetween = rowsWithoutGalaxies.subSet(minRow, maxRow).size();
+        var colExpansionBetween = colsWithoutGalaxies.subSet(minCol, maxCol).size();
+        return Math.abs(gal1.row() - gal2.row()) + Math.abs(gal1.col() - gal2.col()) + rowExpansionBetween
+                + colExpansionBetween;
+    }
+
+    record Position(int row, int col) {
+    }
+
+}
